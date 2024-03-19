5197f15cc785168f994bfd248133b03bf54fc65c
CALIENDO FRANCOIS <francoiscaliendo@gmail.com>
2024-03-19 13:38:20 +0100

    Bon début v1

---
+++ app/src/main/java/fr/isen/caliendo/androiderestaurant/model/DataResult.kt
@@ -0,0 +1,10 @@
+package fr.isen.caliendo.androiderestaurant.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class DataResult (
+
+  @SerializedName("data" ) var data : ArrayList<Data> = arrayListOf()
+
+)
