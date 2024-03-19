d52d0dfc4ab78c5e2d5d69d88cd7fe47f71d29d7
CALIENDO FRANCOIS <francoiscaliendo@gmail.com>
2024-03-19 07:15:27 +0100

    adding

---
+++ app/src/main/java/fr/isen/caliendo/androiderestaurant/DetailsDishesActivity.kt
@@ -0,0 +1,57 @@
+package fr.isen.caliendo.androiderestaurant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import fr.isen.caliendo.androiderestaurant.ui.theme.AndroidERestaurantTheme
+
+class DetailsDishesActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Récupérer le nom du plat de l'intention
+        val dishName = intent.getStringExtra("dishName") ?: "Plat Inconnu"
+
+        setContent {
+            AndroidERestaurantTheme {
+                // Utiliser Surface pour configurer le thème de l'arrière-plan
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Appeler Greeting2 avec le nom du plat
+                    Greeting2(dishName)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 32.dp)
+        )
+    }
+}
